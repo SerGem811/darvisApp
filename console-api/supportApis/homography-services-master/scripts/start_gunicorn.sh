ef1b2102ebb660935d1e290fcb69e2f023466a3b
serGem811 <ser.gem.811@outlook.com>
2019-11-06 04:47:36 -0500

    remove unecessary Supportapi

--- console-api/supportApis/homography-services-master/scripts/start_gunicorn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd  "$(dirname "$0")/.."
-
-[ -e .env ] && rm .env  # remove the .env file if it exists so we pickup any changes
-
-FLASK_APP="homography.flask_server:app"
-
-pipenv run gunicorn -c config/gunicorn.cfg ${FLASK_APP}
